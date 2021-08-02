4471c497fb9bd2d01ea92b592e0c4650b70025af
Nicksa <e.nicksa94@gmail.com>
2021-08-02 20:02:11 +0200

    ajout fonction get et display

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,1 +1,31 @@
-console.log ("bonjour")
+(async function() {
+    const articles = await getArticles()
+
+    for (article of articles) {
+        displayArticle(article)
+    }
+})()
+
+function getArticles() {
+    return fetch("http://localhost:3000/api/teddies")
+        .then(function(httpBodyResponse) {
+            return httpBodyResponse.json()
+        })
+        .then(function(articles){
+            return articles
+        })
+        .catch(function(error) {
+            alert(error)
+        })
+}
+
+function displayArticle(article) {
+    const templateElt = document.getElementById("templateArticle")
+    const cloneElt = document.importNode(templateElt.content, true)
+
+    cloneElt.getElementById("nounours_title").textContent = article.name
+    cloneElt.getElementById("nounours_price").textContent = article.price
+    cloneElt.getElementById("nounours_img").src = article.imageUrl
+
+    document.getElementById("main").appendChild(cloneElt)
+}
